dc3a8328bb578a3de7d982edcd3f8fbe1979fc40
bdh54 <bdh54@miami.edu>
2022-11-03 14:19:38 -0400

    Finished

--- src/ExceptionalIntegers.java
+++ src/ExceptionalIntegers.java
@@ -5,25 +5,46 @@ public class ExceptionalIntegers {
 public static void main(String[] args) {
 
     ArrayList<Integer> intList = new ArrayList<>();
+    int argsLength = args.length;
+    int index;
+    String argsString;
+    Integer wrapperValue;
 
-    argsCheck(intList, args);
 
-    intList
+    for (index = 0; index < argsLength; ++index) {
+        try {
+            argsString = args[index];
+            wrapperValue = converter(argsString);
+            intList.add(wrapperValue);
+        }catch(NumberFormatException e){
+            System.out.println("Catch block says the argument " + "\"" + args[index] +  "\""+ " is ignored because " + args[index]);
     }
-    public static ArrayList<Integer> argsCheck(ArrayList<Integer> intList, String[] args) throws NumberFormatException {
-        int argsLength;
-        int index;
-        argsLength = args.length;
+}
+
+    printContents(intList);
+
+    }
+
+    public static Integer converter( String argsString) throws NumberFormatException {
+
         Integer wrapperValue;
-        for (index=0; index<argsLength; ++index) {
-            try {
-              wrapperValue = Integer.parseInt(args[index]);
-            } catch (NumberFormatException e) {
+        int argsValue;
 
-            }
+              argsValue = Integer.parseInt(argsString);
+              wrapperValue = argsValue;
+              System.out.println("Converter method says integer OK - " + argsValue);
 
+        return(wrapperValue);
+    }
+
+    public static void printContents(ArrayList<Integer> intList) {
+
+        int index;
+
+        System.out.println("\nVector contents are:");
+        for (index=0; index<intList.size(); ++index) {
+            System.out.println("Item " + index + " is " + intList.get(index));
         }
-        return(intList);
     }
 }
 
